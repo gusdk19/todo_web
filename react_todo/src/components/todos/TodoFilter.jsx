2778a185b55a2cb29fe4e26eeff0a25e50df7c67
gusdk19 <hyuna030619@naver.com>
2024-09-12 18:15:09 +0900

    move repository

---
+++ react_todo/src/components/todos/TodoFilter.jsx
@@ -0,0 +1,26 @@
+import { TODO_CATEGORY_ICON } from '@/constants/icon'
+import { useTodos, useTodosDispatch } from '@/contexts/TodoContext';
+
+const TodoFilter = () => {
+
+  const todos = useTodos();
+  const dispatch = useTodosDispatch();
+  const filterTodos = (event) => dispatch({ type: 'FILTER', selectedCategory: event.target.value });
+
+  const url = "localhost:8080/api/todos"
+  const response = fetch();
+
+  return (
+    <select className="p-2 text-gray-100 bg-gray-800 rounded"
+            data-cy="todo-filter"
+            value={todos.category} onChange={filterTodos}
+            >
+      <option value="ALL">All</option>
+      <option value="TODO">{TODO_CATEGORY_ICON.TODO} To do</option>
+      <option value="PROGRESS">{TODO_CATEGORY_ICON.PROGRESS} On progress</option>
+      <option value="DONE">{TODO_CATEGORY_ICON.DONE} Done</option>
+    </select>
+  )
+}
+
+export default TodoFilter
